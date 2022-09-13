5ff30158764c6b2bd81480ada8bfbdbc6139be10
lvhaizhou <417703682@qq.com>
2022-09-13 18:21:39 +0800

    feat(project): learn-log

--- src/Component.js
+++
@@ -1,6 +0,0 @@
-export class Component {
-  static isReactComponent = true;
-  constructor(props) {
-    this.props = props;
-  }
-}
--- src/constants.js
+++
@@ -1,4 +0,0 @@
-// React元素
-export const REACT_ELEMENT = Symbol('react.element');
-
-export const REACT_TEXT = Symbol('react.text');
