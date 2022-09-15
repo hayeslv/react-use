9ef17a43769233b56e07b3211085bc63eb9b77a7
lvhaizhou <417703682@qq.com>
2022-09-15 16:24:06 +0800

    feat(react-app): 引入font-awesome

--- project/react-app/src/components/UI/Counter/Counter.js
+++ project/react-app/src/components/UI/Counter/Counter.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import classes from "./Counter.module.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 export default function Counter(props) {
   const { amount } = props;
@@ -10,14 +12,14 @@ export default function Counter(props) {
         (props.amount && props.amount !== 0)
           ? <>
             <button className={classes.Sub}>
-              <span>-</span>
+            <FontAwesomeIcon icon={faMinus} />
             </button>
             <span className={classes.count}>{ amount }</span>
           </>
           : null
       }
       <button className={classes.Add}>
-        <span>+</span>
+        <FontAwesomeIcon icon={faPlus} />
       </button>
     </div>
   )
--- project/react-app/src/index.js
+++ project/react-app/src/index.js
@@ -6,6 +6,9 @@ import "./index.css"
 // 设置移动端的适配
 document.documentElement.style.fontSize = 100 / 750 + "vw";
 
+// 引入FontAwesome依赖
+// npm i -S @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/free-regular-svg-icons @fortawesome/react-fontawesome@latest
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
