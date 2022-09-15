6e97ff88cc8ca3e129419242d6addc3f9827eeac
lvhaizhou <417703682@qq.com>
2022-09-15 11:11:44 +0800

    feat(react-app): 移动端rem适配

--- project/react-app/src/App.js
+++ project/react-app/src/App.js
@@ -3,9 +3,6 @@ import classes from "./App.module.css"
 
 export default function App() {
   return (
-    <div>
-      <p className={classes.p1}>我是一个段落</p>
-      <button>点我一下</button>
-    </div>
+    <div style={{ width: "750rem", height: 200, backgroundColor: "#bfa" }}>111</div>
   )
 }
