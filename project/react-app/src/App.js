ec1e14f170c5ac938020c5c291e6919a757dbca8
lvhaizhou <417703682@qq.com>
2022-09-15 10:32:48 +0800

    feat(react-app): css-module

---
+++ project/react-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from "./App.module.css"
+
+export default function App() {
+  return (
+    <div>
+      <p className={classes.p1}>我是一个段落</p>
+      <button>点我一下</button>
+    </div>
+  )
+}
