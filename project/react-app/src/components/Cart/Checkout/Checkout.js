ca684f9a8f077b38f9ed4a25082ed8635a69ed73
lvhaizhou <417703682@qq.com>
2022-09-16 10:54:03 +0800

    feat(react-app): 结账页面-商品详情

--- project/react-app/src/components/Cart/Checkout/Checkout.js
+++ project/react-app/src/components/Cart/Checkout/Checkout.js
@@ -1,15 +1,33 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
-import React from 'react'
+import React, { useContext } from 'react'
 import ReactDOM from "react-dom"
 import classes from './Checkout.module.css';
+import CartContext from "../../../store/cart-context"
+import CheckoutItem from './CheckoutItem/CheckoutItem';
 
 const checkoutRoot = document.getElementById("checkout-root")
 
 export default function Checkout(props) {
+  const ctx = useContext(CartContext)
+
   return ReactDOM.createPortal(<div className={classes.Checkout}>
     <div className={classes.Close}>
       <FontAwesomeIcon icon={faXmark} onClick={() => props.onHide()} />
+    </div>
+
+    <div className={classes.MealsDesc}>
+      <header className={classes.Header}>
+        <h2 className={classes.Title}>餐品详情</h2>
+      </header>
+
+      <div className={classes.Meals}>
+        {ctx.items.map(item => <CheckoutItem key={item.id} meal={item}></CheckoutItem>)}
+      </div>
+
+      <footer className={classes.Footer}>
+        <p className={classes.TotalPrice}>{ctx.totalPrice}</p>
+      </footer>
     </div>
   </div>, checkoutRoot)
 }
